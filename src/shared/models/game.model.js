08bf3b6d8e0df96d87d9a0a4c457623bf5d177a7
AurelieBnt <aurelie.bonnot@hotmail.com>
2019-10-29 17:00:29 +0100

    Create Class

---
+++ src/shared/models/game.model.js
@@ -0,0 +1,16 @@
+export class Game{
+
+    constructor () {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.date = date;
+        this.adress = adress;
+        this.city = city;
+        this.maxPlayers = maxPlayers;
+        this.minPlayers = minPlayers
+        this.players = players;
+    }
+
+}
--- src/shared/models/user.model.js
+++ src/shared/models/user.model.js
@@ -1,9 +1,15 @@
+import {UserAdress} from "./user-adress.model";
+import {UserInformations} from "./user-informations.model";
+
 export class User {
+
 
     constructor (email, password, surname) {
         this.email = email;
         this.password = password;
         this.surname = surname; 
+        this.userAdress = new UserAdress();
+        this.userInformations = new UserInformations();
     };
 
 }
