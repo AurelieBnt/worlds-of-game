06c703ab7feb9d06c4e19c585303d134d63c9833
AurelieBnt <aurelie.bonnot@hotmail.com>
2019-10-30 17:03:25 +0100

    Update class

--- src/shared/components/register-button.component.js
+++ src/shared/components/register-button.component.js
@@ -4,16 +4,15 @@ export class RegisterButton extends Components {
 
     constructor (textNode, link) {
         super();
-        this.registerButtonLink;
         this.link = link;
         this.textNode = textNode;
     }
 
     display (div1) {
-        this.registerButtonLink = super.createAppendElement(div1, "a");
-        const registerButton = super.createAppendElement(this.registerButtonLink, "button");
+        const registerButtonLink = super.createAppendElement(div1, "a");
+        const registerButton = super.createAppendElement(registerButtonLink, "button");
         const registerButtonText = super.createTextAndAppendElement(registerButton, this.textNode);
-        super.setAttribute(this.registerButtonLink, {href : this.link});
+        super.setAttribute(registerButtonLink, {href : this.link});
         super.setAttribute(registerButton, {type : "submit"});
     }
 
