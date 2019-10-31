8336b9e6819b90d72a42b28408f604a9a82c648d
Aurélie <aurelie.bonnot@hotmail.com>
2019-10-31 15:36:00 +0100

    Create model

---
+++ src/shared/models/input.model.js
@@ -0,0 +1,11 @@
+export class Input{
+
+    constructor (id, name, value, placeholder, type) {
+        this.id = id;       
+        this.name = name;
+        this.value = value
+        this.placeholder = placeholder;
+        this.type = type;
+    }
+
+}
