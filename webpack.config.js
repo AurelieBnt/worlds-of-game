6e334b4dc81328bb68a8fb6c8e1b3b3f277b1b52
Aurélie <aurelie.bonnot@hotmail.com>
2019-10-31 12:04:38 +0100

    Add boostrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,53 @@
 
-const path = require("path");
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: [
+        './src/index.js',
+        './src/index.scss'
+    ],
     output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "www/dist"),
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'www/dist'),
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }]
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif|woff|woff2|eot|ttf|svg)$/,
+                loader: 'file-loader'
+            }
+        ]
     },
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'index.css',
+        }),
         new BrowserSyncPlugin({
-            host: "localhost",
+            host: 'localhost',
             port: 9000,
             server: {
                 baseDir: [
-                    "./www/"
+                    './www/'
                 ]
             },
             files: [
-                "www/index.html"
+                'www/index.html'
             ]
         })
     ],
