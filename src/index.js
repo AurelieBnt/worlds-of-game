76402dce90283630c66cc0f55d975ddb0415aa62
Aurélie <aurelie.bonnot@hotmail.com>
2019-10-31 15:27:23 +0100

    Update code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { Login } from "./app/login/login.component";
+import { Login, LoginComponent } from "./app/login/login.component";
 import {User} from "./shared/models/user.model";
 
-const login = new Login(new User("licorne","bisournours"));
-login.display();
+const loginComponent = new LoginComponent(new User("licorne","bisournours"));
+loginComponent.display();
 
