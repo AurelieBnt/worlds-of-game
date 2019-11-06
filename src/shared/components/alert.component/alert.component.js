1139d1ef5f0abb5e1aa3bb3a09bf82501d2b9662
Aurélie <aurelie.bonnot@hotmail.com>
2019-11-06 15:59:30 +0100

    Create class

---
+++ src/shared/components/alert.component/alert.component.js
@@ -0,0 +1,22 @@
+import { Components } from "../super-class/super.component";
+
+export class AlertComponent extends Components{
+
+    constructor (text) {
+        super();  
+        this.text = text;
+        this.element = null;
+    }
+    
+    display (parent) {
+    this.element = super.createAppendElement(parent, "label");    
+    super.createTextAndAppendElement(this.element, this.text);
+    parent.insertBefore(this.element, parent.firstChild);
+    //super.setAttribute(this.element, {name : this.name, value : this.value});
+    }
+
+    hide(){
+        this.element.parentNode.removeChild(this.element);
+    }
+
+}
