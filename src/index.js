3f7c77a48b94d21239d61268cbfa85652bea0b15
Aurélie <aurelie.bonnot@hotmail.com>
2019-11-07 12:21:57 +0100

    Update code

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import { Login, LoginComponent } from "./app/login/login.component";
 import {User} from "./shared/models/user.model";
 import { RegisterComponent } from "./app/register/register.component";
 import { UserService } from "./shared/services/user-services/user.services";
+import { HomeComponent } from "./app/home/home.component";
 
-// const loginComponent = new LoginComponent();
-// loginComponent.display();
+const loginComponent = new LoginComponent();
+loginComponent.display(document.body);
 
-const registerComponent = new RegisterComponent("Register");
-registerComponent.display(document.body);
+// const registerComponent = new RegisterComponent("Register");
+// registerComponent.display(document.body);
+
+// const homeComponent = new HomeComponent();
+// homeComponent.display(document.body);
 
