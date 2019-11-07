641450d61d5c7aa20bb7833214293b87cee24620
Aurélie <aurelie.bonnot@hotmail.com>
2019-11-07 18:46:32 +0100

    Create back button

---
+++ src/shared/components/back-button.component/back-button.component.js
@@ -0,0 +1,30 @@
+import { Components } from "../super-class/super.component";
+import { Router } from "../../services/router.services/router.services";
+
+export class BackButtonComponent extends Components {
+
+    constructor(text, type, name){
+        super()
+        this.text = text;
+        this.type = type;
+        this.name = name;
+        this.router = new Router();
+    }
+
+    display(parent){
+        this.button = super.createAppendElement(parent, "button");
+        super.createTextAndAppendElement(this.button, this.text);
+        super.setAttribute(this.button, {type : this.type, id: this.text});
+        this.button.addEventListener("click", (event) => {this.clickButton(event);})
+    }
+
+    hide(){
+        this.button.parentNode.removeChild(this.button);
+    }
+
+    clickButton(){
+        this.router.navigate(this.name);
+    }
+
+
+}
