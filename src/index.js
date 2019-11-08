df1f2fa74ff1aed29a16db6a60570ad44641b839
Aurélie <aurelie.bonnot@hotmail.com>
2019-11-08 21:57:22 +0100

    Update code

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import { RegisterComponent } from "./app/register/register.component";
 import { HomeComponent } from "./app/home/home.component";
 import { Router } from "./shared/router.services/router.services";
 
-
-//loginComponent.display(document.body);
-
 Router.addComponent("login", "/login", new LoginComponent());
 Router.addComponent("register", "/register", new RegisterComponent("Register"));
 Router.addComponent("home", "/home", new HomeComponent());
 
 Router.navigate("login");
 
-//router.addComponent("login", "/login", loginComponent);
+
 
 
 
